d07cf534da5e245d5494ca017b54981e58f7dd5b
EtzelMV <etzelvalderrama@gmail.com>
2023-06-25 23:16:04 -0400

    Grupal 01 listo

---
+++ src/main/java/controlador/Inicio.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Inicio
+ */
+public class Inicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Inicio() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Obtener el objeto RequestDispatcher para el JSP
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+
+		// Redirigir la solicitud al JSP
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
