e4e8cfbe7e8fb84ff7e83f9dc6d980f80e742738
EtzelMV <etzelvalderrama@gmail.com>
2023-06-25 23:16:04 -0400

    Grupal 01 listo

---
+++ src/main/java/controlador/CrearCapacitacion.java
@@ -0,0 +1,45 @@
+package controlador;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CrearCapacitacion
+ */
+public class CrearCapacitacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CrearCapacitacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Obtener el objeto RequestDispatcher para el JSP
+		RequestDispatcher dispatcher = request.getRequestDispatcher("views/crearCapacitacion.jsp");
+
+		// Redirigir la solicitud al JSP
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/controlador/CriterioEvaluacion.java
@@ -0,0 +1,46 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CriterioEvaluacion
+ */
+public class CriterioEvaluacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CriterioEvaluacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Configura la respuesta para mostrar el contenido del PDF
+        response.setContentType("application/pdf");
+
+        // Obtén la ruta del archivo PDF
+        String rutaPDF = "files/CriterioDeEvaluacion1.pdf";
+
+        // Envía el archivo PDF como respuesta
+        response.sendRedirect(rutaPDF);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
